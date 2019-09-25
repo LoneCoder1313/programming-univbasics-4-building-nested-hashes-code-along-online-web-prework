aa0cac86895d20d7512a5c5027a726ddad5e6eb9
Ricky dildy <rdildy92@gmail.com>
2019-09-25 14:20:46 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,10 +1,10 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch =>{},
-      :matriarch =>{},
-      :hero =>{},
-      :hero_friend =>[]
+      :patriarch => "Lord montague"
+      :matriarch => "Lady montague"
+      :hero => "Ricaris"
+      :hero_friend =>
       }, 
     :capulet => {
       :patriarch =>{},
